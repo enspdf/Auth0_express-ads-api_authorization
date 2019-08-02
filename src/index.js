d5782c596f9043b83123d3cc8b25ba331bc86a36
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-08-02 00:32:45 -0500

    express ads api

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const jwt = require("express-jwt");
+const jwtKsRsa = require("jwks-rsa");
+const { startDatabase } = require("./database/mongo");
+const { getAds, insertAd, deleteAd, updateAd } = require("./database/ads");
+
+const app = express();
+
+const ads = [
+  {
+    title: "Hello World"
+  }
+];
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+app.get("/", async (req, res) => {
+  res.send(await getAds());
+});
+
+app.post("/", async (req, res) => {
+  const newAd = req.body;
+  await insertAd(newAd);
+
+  res.send({ message: "New ad inserted." });
+});
+
+app.delete("/:id", async (req, res) => {
+  await deleteAd(req.params.id);
+  res.send({ message: "Ad removed." });
+});
+
+app.put("/:id", async (req, res) => {
+  const updatedAd = req.body;
+  await updateAd(req.params.id, updatedAd);
+
+  res.send({ message: "Ad updated." });
+});
+
+startDatabase().then(async () => {
+  await insertAd({ title: "In-memory database" });
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+});
